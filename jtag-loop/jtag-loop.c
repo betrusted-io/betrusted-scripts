0ad792e5d025d0590c384170fe07462bcda8e6a2
bunnie <bunnie@kosagi.com>
2019-12-05 13:04:29 +0800

    add debugging; fix bugs. now able to do idcode loop

--- jtag-loop/jtag-loop.c
+++ jtag-loop/jtag-loop.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 #include <stdint.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <termios.h>
+#include <unistd.h>
 
 #include "rpi.h"
 
@@ -15,6 +16,8 @@
 #define J_TMS  17
 #define J_RESET 24
 #define J_SEL  18
+
+#define DEBUG_JTAG 0
 
 enum jtag_pin {
   JT_TCK,
@@ -99,23 +102,67 @@ int main(int argc, char **argv) {
   jtagSetPin(jtag, JT_RESET, J_RESET);
   jtagSetPin(jtag, JT_SEL, J_SEL);
   
-  sfd = open("/dev/ttyS0", O_RDWR);
+  sfd = open("/dev/ttyS0", O_RDWR | O_NOCTTY);
 
   if( sfd == -1 ) {
     fprintf( stderr, "Fatal error opening serial port\n");
     return 1;
   }
 
+  /*---------- Setting the Attributes of the serial port using termios structure --------- */
+
+  struct termios SerialPortSettings;/* Create the structure                          */
+
+  tcgetattr(sfd, &SerialPortSettings);/* Get the current attributes of the Serial port */
+
+  /* Setting the Baud rate */
+  cfsetispeed(&SerialPortSettings,B115200); /* Set Read  Speed as 115200                       */
+  cfsetospeed(&SerialPortSettings,B115200); /* Set Write Speed as 115200                       */
+
+  /* 8N1 Mode */
+  SerialPortSettings.c_cflag &= ~PARENB;   /* Disables the Parity Enable bit(PARENB),So No Parity   */
+  SerialPortSettings.c_cflag &= ~CSTOPB;   /* CSTOPB = 2 Stop bits,here it is cleared so 1 Stop bit */
+  SerialPortSettings.c_cflag &= ~CSIZE; /* Clears the mask for setting the data size             */
+  SerialPortSettings.c_cflag |=  CS8;      /* Set the data bits = 8                                 */
+
+  SerialPortSettings.c_cflag &= ~CRTSCTS;       /* No Hardware flow Control                         */
+  SerialPortSettings.c_cflag |= CREAD | CLOCAL; /* Enable receiver,Ignore Modem Control lines       */
+
+
+  SerialPortSettings.c_iflag &= ~(IXON | IXOFF | IXANY);          /* Disable XON/XOFF flow control both i/p and o/p */
+  SerialPortSettings.c_iflag &= ~(ICANON | ECHO | ECHOE | ISIG);  /* Non Cannonical mode                            */
+
+  SerialPortSettings.c_oflag &= ~OPOST;/*No Output Processing*/
+
+  /* Setting Time outs */
+  SerialPortSettings.c_cc[VMIN] = 1; /* Read at least 1 characters */
+  SerialPortSettings.c_cc[VTIME] = 0; /* Wait indefinetly   */
+
+
+  if((tcsetattr(sfd,TCSANOW,&SerialPortSettings)) != 0) /* Set the attributes to the termios structure*/
+    fprintf(stderr, "\n  ERROR ! in Setting attributes");
+  /*  else
+      printf("\n  BaudRate = 115200 \n  StopBits = 1 \n  Parity   = none");*/
+
+
   jtagInit(jtag);
   
   unsigned char c, bits, ret;
   ssize_t count;
+  
+  tcflush(sfd, TCIFLUSH);   /* Discards old data in the rx buffer            */
   while(1) {
     count = read(sfd, &c, 1);
       
     if( count != 1 ) {
       fprintf( stderr, "unexpected count return, continuing\n" );
     }
+    if( c == 0 ) {
+      // ignore null reads
+      continue;
+    }
+    //putchar(c);
+    //fflush(stdout);
 
     // characters starting at offset '@' are direct set of pin state (including clock pin)
     // a read data is done immediately upon conclusion of setting pin state: we count on the FPGA being fast
@@ -126,6 +173,11 @@ int main(int argc, char **argv) {
     // every charater sent returns a character which is the line state at the conclusion of the command
     bits = 0xf & c;
     if ((c & 0xf0) == 0x40) {  // 0x40 = '@'
+      if (DEBUG_JTAG) {
+	putchar('*');
+	fflush(stdout);
+      }
+
       bits & MASK_TMS ? gpioWrite(jtag->pins.tms, 1) : gpioWrite(jtag->pins.tms, 0);
       bits & MASK_TDI ? gpioWrite(jtag->pins.tdi, 1) : gpioWrite(jtag->pins.tdi, 0);
 
@@ -142,6 +194,16 @@ int main(int argc, char **argv) {
       bits & MASK_TMS ? gpioWrite(jtag->pins.tms, 1) : gpioWrite(jtag->pins.tms, 0);
       bits & MASK_TDI ? gpioWrite(jtag->pins.tdi, 1) : gpioWrite(jtag->pins.tdi, 0);
 
+      if (DEBUG_JTAG) {
+	if( bits & MASK_TMS ) { putchar('M'); } else { putchar('m'); }
+	if( bits & MASK_TDI ) { putchar('D'); } else { putchar('d'); }
+	fflush(stdout);
+      }
+
+      // sample the TDO going *before* pulsing clock: this is because when we are in
+      // the "shift" state, TDO is immediately valid.
+      ret = gpioRead(jtag->pins.tdo) ? '1' : '0';
+      
       // 3.00ns/2.00ns set/hold on TDI/TMS to jtag rising edge
       gpioRead(jtag->pins.tck);
       jtagPause(jtag);
@@ -156,12 +218,21 @@ int main(int argc, char **argv) {
       gpioRead(jtag->pins.tck);
       jtagPause(jtag);
 
-      ret = gpioRead(jtag->pins.tdo) ? '1' : '0';
+      if (DEBUG_JTAG) {
+	putchar(ret);
+	fflush(stdout);
+      }
+    } else {
+      if (DEBUG_JTAG) {
+	printf("%02x ", c);
+	fflush(stdout);
+      }
     }
 
     // return the state of the TDO pin
     if( write(sfd, &ret, 1) != 1 ) {
       fprintf(stderr, "return write failed\n");
     }
+    syncfs(sfd);
   }
 }
