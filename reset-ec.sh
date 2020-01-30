11af1d48f6eb45e661b3bd693d86d5f337d7ec7c
bunnie <bunnie@kosagi.com>
2020-01-30 11:29:59 +0000

    add scripts to reset EC and SOC
    SOC script is particularly important with DOPI mode on ROM!
    You will need to hard reset the system to reconfigure the ROM now.

---
+++ reset-ec.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+if [ ! -d /sys/class/gpio/gpio25 ]
+then
+    echo "25" > /sys/class/gpio/export
+fi
+
+echo "out" > /sys/class/gpio/gpio25/direction
+echo 0 > /sys/class/gpio/gpio25/value
+sleep 0.1
+echo 1 > /sys/class/gpio/gpio25/value
+
---
+++ reset-soc.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+if [ ! -d /sys/class/gpio/gpio24 ]
+then
+    echo "24" > /sys/class/gpio/export
+fi
+
+echo "out" > /sys/class/gpio/gpio24/direction
+echo 0 > /sys/class/gpio/gpio24/value
+sleep 0.1
+echo 1 > /sys/class/gpio/gpio24/value
+
