27eaf4809640fc588b0b3abd2447d38417c596e6
bunnie <bunnie@kosagi.com>
2020-01-30 09:19:52 +0000

    add a version of vbus that just takes an argument
    0 = off
    1 = on

---
+++ vbus.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+if [ "$1" != "0" ] && [ "$1" != "1" ]; then
+    echo "Needs an argument of 0 or 1"
+    exit 0
+fi
+
+if [ ! -d /sys/class/gpio/gpio21 ]
+then
+    echo "21" > /sys/class/gpio/export
+fi
+
+echo "out" > /sys/class/gpio/gpio21/direction
+echo $1 > /sys/class/gpio/gpio21/value
