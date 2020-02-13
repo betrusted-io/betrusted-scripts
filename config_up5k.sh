c4dc037648fe42eb534f29e55372cd0e2feab83f
bunnie <bunnie@kosagi.com>
2020-02-13 11:57:09 +0000

    make sure the QE bit is set on the FLASH
    this way images that rely upon quad spi can work correctly.
    
    requires latest version of fomu-flash from betrusted repo
    (or upstream version)

--- config_up5k.sh
+++ config_up5k.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sudo ../fomu-flash/fomu-flash -w bt-ec.bin && sudo ../fomu-flash/fomu-flash -r
+sudo ../fomu-flash/fomu-flash -4 && sudo ../fomu-flash/fomu-flash -w bt-ec.bin && sudo ../fomu-flash/fomu-flash -r 
