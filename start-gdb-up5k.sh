03ba76ce4214826b1a5fe88467e0fd37570e3500
bunnie <bunnie@kosagi.com>
2020-02-19 18:55:30 +0000

    update gdb script to include crossover args and csv pointer

--- start-gdb-up5k.sh
+++ start-gdb-up5k.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-wishbone-tool --uart /dev/ttyS0 -b 115200 -s gdb --bind-addr 0.0.0.0
+wishbone-tool --uart /dev/ttyS0 -b 115200 -s gdb --bind-addr 0.0.0.0 -s terminal --csr-csv=ec-csr.csv
