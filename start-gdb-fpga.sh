448fdbf2cf383992d5aaecd4ed0b59c1af8b2bbe
bunnie <bunnie@kosagi.com>
2020-10-15 19:06:20 +0100

    stash progress debugging native programming

--- start-gdb-fpga.sh
+++ start-gdb-fpga.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-wishbone-tool --uart /dev/ttyS0 -b 115200 -s gdb --bind-addr 0.0.0.0 -s terminal --csr-csv=soc-csr.csv --debug-offset=0xefff0000
+wishbone-tool --uart /dev/ttyS0 -b 115200 -s gdb --bind-addr 0.0.0.0 -s terminal --csr-csv=../bin/soc-csr.csv --debug-offset=0xefff0000
