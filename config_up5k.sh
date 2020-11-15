16953cf8b52c70ba15f1681bcf9a5352a3e3e654
bunnie <bunnie@kosagi.com>
2020-11-15 21:13:21 +0000

    update paths

--- config_up5k.sh
+++ config_up5k.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sudo ../fomu-flash/fomu-flash -4 && sudo ../fomu-flash/fomu-flash -w ../bin/bt-ec.bin && sudo ../fomu-flash/fomu-flash -r 
+sudo ../fomu-flash/fomu-flash -4 && sudo ../fomu-flash/fomu-flash -w ../precursors/bt-ec.bin && sudo ../fomu-flash/fomu-flash -r 
--- start-gdb-up5k.sh
+++ start-gdb-up5k.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-wishbone-tool --uart /dev/ttyS0 -b 115200 -s gdb --bind-addr 0.0.0.0 -s terminal --csr-csv=../bin/ec-csr.csv
+wishbone-tool --uart /dev/ttyS0 -b 115200 -s gdb --bind-addr 0.0.0.0 -s terminal --csr-csv=../precursors/ec-csr.csv
