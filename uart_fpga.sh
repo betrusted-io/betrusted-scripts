8e5005d2dfd1bc3c6bb084e0be5b9ac61b33398f
Sam Blenny <68084116+samblenny@users.noreply.github.com>
2021-04-27 02:41:23 +0000

    wait for gpio device to spawn before echo
    Without the sleep, these scripts can give errors like this:
    ./reset-ec.sh: 8: ./reset-ec.sh: cannot create /sys/class/gpio/gpio25/direction: Permission denied
    ./reset-ec.sh: 9: ./reset-ec.sh: cannot create /sys/class/gpio/gpio25/value: Permission denied
    ./reset-ec.sh: 11: echo: echo: I/O error

--- uart_fpga.sh
+++ uart_fpga.sh
@@ -5,5 +5,6 @@ then
     echo "18" > /sys/class/gpio/export
 fi
 
+sleep 0.1
 echo "out" > /sys/class/gpio/gpio18/direction
 echo "1" > /sys/class/gpio/gpio18/value
--- vbus_off.sh
+++ vbus_off.sh
@@ -5,5 +5,6 @@ then
     echo "21" > /sys/class/gpio/export
 fi
 
+sleep 0.1
 echo "out" > /sys/class/gpio/gpio21/direction
 echo "0" > /sys/class/gpio/gpio21/value
