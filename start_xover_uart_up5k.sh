937d2400f1280cd4c0d03b6664eae014f36755fe
bunnie <bunnie@kosagi.com>
2021-08-25 14:58:50 +0100

    fix underscore in script

--- start_xover_uart_up5k.sh
+++ start_xover_uart_up5k.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -x
 ./uart_up5k.sh
-./reset-ec.sh \
+./reset_ec.sh \
     && sleep 0.1 \
     && wishbone-tool --uart /dev/serial0 -b 115200 -s terminal --csr-csv=../precursors/csr.csv
