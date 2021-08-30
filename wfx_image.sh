79f343ed19ef88dd699ace76971ecc036fc83c0d
bunnie <bunnie@kosagi.com>
2021-08-30 09:49:01 +0100

    remove second reference to wf200, source from betrusted-ec build tree via precursors

--- wfx_image.sh
+++ wfx_image.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sudo ../fomu-flash/fomu-flash -w wfx-firmware/wfm_wf200_C0.sec -a 0x9C000 && sudo ../fomu-flash/fomu-flash -r 
+sudo ../fomu-flash/fomu-flash -w ../precursors/wfm_wf200_C0.sec -a 0x9C000 && sudo ../fomu-flash/fomu-flash -r 
