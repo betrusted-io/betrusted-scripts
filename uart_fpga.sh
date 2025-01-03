337f3ed1b1ff0b5cbc673edfa6253f97fc65afb7
Peter Lebbing <peter@digitalbrains.com>
2025-01-03 15:42:58 +0100

    Switch sysfs GPIO ABI to pigpiod
    The sysfs GPIO ABI has been deprecated in 2015 and Raspberry Pi OS
    bookworm no longer supports it.

--- uart_fpga.sh
+++ uart_fpga.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 
-if [ ! -d /sys/class/gpio/gpio18 ]
-then
-    echo "18" > /sys/class/gpio/export
-fi
+. ./functions.sh
 
-sleep 0.1
-echo "out" > /sys/class/gpio/gpio18/direction
-echo "1" > /sys/class/gpio/gpio18/value
+ensure_pigpiod
+pigs modes 18 w write 18 1
--- vbus_off.sh
+++ vbus_off.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 
-if [ ! -d /sys/class/gpio/gpio21 ]
-then
-    echo "21" > /sys/class/gpio/export
-fi
+. ./functions.sh
 
-sleep 0.1
-echo "out" > /sys/class/gpio/gpio21/direction
-echo "0" > /sys/class/gpio/gpio21/value
+ensure_pigpiod
+pigs modes 21 w write 21 0
